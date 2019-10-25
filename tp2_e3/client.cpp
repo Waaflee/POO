a10c3fae54669ee362010870cb7f05995c212fdf
Emanuel <ema.aguero96@gmail.com>
2019-10-25 19:29:10 -0300

    implementacion del cliente

--- tp2_e3/client.cpp
+++ tp2_e3/client.cpp
@@ -6,16 +6,24 @@ using namespace XmlRpc;
 int main(int argc, char* argv[]) {
     std::string opcion = " ";
     float a1,a2;
-    while(opcion!="t"){
+    while(true){
     char opcion;
     std::cout << "=========================================" << std::endl << "CALCULADORA DISTRIBUIDA CON RPC" << std::endl << "=========================================" << std::endl;
     std::cout << "a. Adicion" << std::endl << "s. Sustraccion" << std::endl << "m. Multiplicacion" << std::endl << "d. Division" << std::endl << "p. Potenciacion" << std::endl << "r. Radicacion" << std::endl << "h. Historial" << std::endl << "t. Terminar" << std::endl << "*****************************************" << std::endl;
     std::cout << "Opcion: ";
     std::cin >> opcion;
-    std::cout << "Argumento1: ";
-    std::cin >> a1;
-    std::cout << "Argumento2: ";
-    std::cin >> a2;
+    if(opcion != 't'){
+      std::cout << "Argumento1: ";
+      std::cin >> a1;
+      std::cout << "Argumento2: ";
+      std::cin >> a2;
+    }
+    else break;
+    if(opcion == 'd' && a2 == 0){
+      std::cout << "---No se puede dividir por cero---" << std::endl;
+      continue;
+    }
+  // Call the remote procedure Hello and print the result value
     
   if (argc != 3) {
     std::cerr << "Usage: HelloClient serverHost serverPort\n";
@@ -29,9 +37,15 @@ int main(int argc, char* argv[]) {
 
   XmlRpcValue noArgs, result;
 
-noArgs[0] = opcion;
-  // Call the remote procedure Hello and print the result value
-  if (client.execute("sum", noArgs, result))
-    std::cout << result << std::endl;
+noArgs[0] = a1;
+noArgs[1] = a2;
+  if(opcion=='a'){ client.execute("sum", noArgs, result); std::cout << result << std::endl << std::endl;}
+  if(opcion=='s'){ client.execute("res", noArgs, result); std::cout << result << std::endl << std::endl;}
+  if(opcion=='m'){ client.execute("mul", noArgs, result); std::cout << result << std::endl << std::endl;}
+  if(opcion=='d'){ client.execute("div", noArgs, result); std::cout << result << std::endl << std::endl;}
+  if(opcion=='p'){ client.execute("pow", noArgs, result); std::cout << result << std::endl << std::endl;}
+  if(opcion=='r'){ client.execute("nroot", noArgs, result); std::cout << result << std::endl << std::endl;}
+  //if(opcion=='h') client.execute("sum", noArgs, result);
+  
   }
 }
