52f7f92589193508b1d639fe82c6042b58a4bfeb
Waaflee <agustinwafle@gmail.com>
2019-11-01 22:29:01 -0300

    Implemented robotic arm with tween animation

--- RoboticArm/RoboticArm.gd
+++ RoboticArm/RoboticArm.gd
@@ -1,16 +1,65 @@
 extends Spatial
 
-var gravity = -12  # strength of gravity Vector3.DOWN * 12
-var speed = 4  # movement speed
-var jump_speed = 6  # jump strength
-var spin = 1  # rotation speed
+onready var prisma_1 := get_node("Prisma1")
+onready var prisma_2 := get_node("Prisma1").get_node("Prisma2")
+onready var prisma_3 := get_node("Prisma1").get_node("Prisma2").get_node("Prisma3")
+onready var tween := get_node("Tween")
+var pinza_abierta = false
+var grados_1 := 0
+var grados_2 := 0
+var grados_3 := 0
 
 func _ready():
 	pass
 
 func _process(delta):
-#	get_node("Prisma1").rotate_z(-delta * 2)rotate_object_local(Vector3(1, 0, 0), PI)
-#	get_node("Prisma1").get_node("Prisma2").rotate_z(delta * 10)
-#	get_node("Prisma1").get_node("Prisma2").get_node("Prisma3").rotate_x(delta * 0.5)
-#	get_node("Prisma1").get_node("Prisma2").get_node("Prisma3").rotate_object_local(Vector3(1, 0, 0), 0.01)
-	pass
+	if Input.is_action_just_pressed("ui_accept"):
+		toggle_pinza()
+	if Input.is_action_just_pressed("ui_up"):
+		grados_1 += 5
+		grados_1 = clamp(grados_1, -90, 90)
+		rotar_articulacion(1, grados_1, 90)
+		print(grados_1)
+	if Input.is_action_just_pressed("ui_down"):
+		grados_1 -= 5
+		rotar_articulacion(1, grados_1, 90)
+		print(grados_1)
+#	pass
+
+func abrir_pinza():
+	pinza_abierta = true
+	tween.interpolate_property(prisma_3.get_node("Pinza1"), "rotation_degrees", prisma_3.get_node("Pinza1").rotation_degrees, Vector3(0, 30, 0), 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN, 0)
+	tween.interpolate_property(prisma_3.get_node("Pinza2"), "rotation_degrees", prisma_3.get_node("Pinza2").rotation_degrees, Vector3(0, -30, 0), 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN, 0)
+
+func cerrar_pinza():
+	pinza_abierta = false
+	tween.interpolate_property(prisma_3.get_node("Pinza1"), "rotation_degrees", prisma_3.get_node("Pinza1").rotation_degrees, Vector3(0, 0, 0), 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN, 0)
+	tween.interpolate_property(prisma_3.get_node("Pinza2"), "rotation_degrees", prisma_3.get_node("Pinza2").rotation_degrees, Vector3(0, 0, 0), 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN, 0)
+	
+func toggle_pinza():
+	if pinza_abierta:
+		cerrar_pinza()
+	else:
+		abrir_pinza()
+	tween.start()
+
+func rotar_articulacion(articulacion: int, grados: float, speed: float = 45):
+		grados = clamp(grados, -90, 90)
+		if speed == 0:
+			speed = 45
+		var time = abs(grados / speed) + 1
+		if articulacion == 1:
+			tween.interpolate_property(prisma_1, "rotation_degrees", prisma_1.rotation_degrees, Vector3(grados, 0 , 0), time, Tween.TRANS_LINEAR, Tween.EASE_IN, 0)
+		if articulacion == 2:
+			tween.interpolate_property(prisma_2, "rotation_degrees", prisma_2.rotation_degrees, Vector3(grados, 0, 0), time, Tween.TRANS_LINEAR, Tween.EASE_IN, 0)
+		if articulacion == 3:
+			tween.interpolate_property(prisma_3, "rotation_degrees", prisma_3.rotation_degrees, Vector3(grados, 0, 0), time, Tween.TRANS_LINEAR, Tween.EASE_IN, 0)
+		tween.start()
+		
+func _on_Timer_timeout():
+	toggle_pinza()
+#	rotar_articulacion(1, 150, 90)
+#	rotar_articulacion(3, 150, 90)
+#	rotar_articulacion(2, 150, 90)
+#	rotar_articulacion(3, count*1.5, 90)
+	
